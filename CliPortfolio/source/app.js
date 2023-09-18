15f382635f87243b4b1db4a6480c996ec5fba637
Jacob Gardner <jacob.gardner@bjss.com>
2023-09-18 10:58:26 +0100

    feat: added languages box and set up flex layout

--- CliPortfolio/source/app.js
+++ CliPortfolio/source/app.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Box, Text} from 'ink';
+
 import DescBox from "./descBox.js";
+import LangBox from "./langBox.js";
 
 const App = () => (
-	<DescBox />
+	<Box borderStyle="single" flexWrap="wrap" alignContent="flex-start" width="90%">
+		<Text color="white" bold underline width="100%">Portfolio</Text>
+		<Box width="100%">
+			<Box flex="3" flex-direction="column">
+				<DescBox />
+			</Box>
+			<LangBox flex="1"/>
+		</Box>
+	</Box>
 );
 
 export default App;
--- CliPortfolio/source/descBox.js
+++ CliPortfolio/source/descBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Text} from "ink";
+import {Box, Text, Newline} from "ink";
 
 const today = new Date();
 const age = today.getFullYear() - 1999;
@@ -9,14 +9,15 @@ if(today.getMonth() < 5 || (today.getMonth == 5 && today.getDay() < 28)) {
 
 const DescBox = () => {
 	return (
-		<Box borderStyle="double" flexDirection="column">
+		<Box borderStyle="double" flexDirection="column" height="45" paddingY="1" paddingX="2">
+			<Text color="#5763d4" bold underline>Overview</Text>
 			<Text>
 				<Text color="cyan" bold>Name: </Text>
 				<Text>Jacob Gardner</Text>
 			</Text>
 			<Text>
 				<Text color="cyan" bold>Age: </Text>
-				<Text>{age}</Text>
+				<Text>{age}<Newline /></Text>
 			</Text>
 			<Text>
 				<Text>I'm a graduate software developer and business analyst for BJSS currently working with TPICAP. I'm passionate about learning new tech and solving complex problems!</Text>
